abdadd27a7a1ec90d31bfb598ab28525e20eae90
Mashashi Nakamura <mashashi.nakamura@gmail.com>
2016-02-12 13:40:33 +0100

    Programmer exceptions can be now caught correctly

---
+++ JavaRoles/tests/pt/mashashi/javaroles/composition/TestOriginalRigidVoidMethod.java
@@ -0,0 +1,56 @@
+package pt.mashashi.javaroles.composition;
+
+import static org.junit.Assert.*;
+
+import pt.mashashi.javaroles.ObjectForRole;
+
+public class TestOriginalRigidVoidMethod {
+	
+	public interface Human{ void hello(); }
+	public interface Monkey{ void hello(); }
+	
+	public static class Portuguese implements Human{
+		private AnimalRoles a;
+		public Portuguese(AnimalRoles a){
+			this.a=a;
+		}
+		@Override
+		public void hello(){
+			this.a.out="Role Portuguese";
+		}
+	}
+	public static class Bonobo implements Monkey{
+		private AnimalRoles a;
+		public Bonobo(AnimalRoles a){
+			this.a=a;
+		}
+		@Override public void hello() {
+			this.a.out="Role Bonobo";
+		} 
+	}
+	public static class AnimalRoles implements Human, Monkey{		
+		@ObjectForRole public Human human;
+		@ObjectForRole public Monkey monkey;
+		@OriginalRigid public Human original;
+		public String out;
+		public AnimalRoles(){
+			human = new Portuguese(this);
+			monkey = new Bonobo(this);
+		}
+		@Override
+		public void hello() {
+			out = "Rigid";
+		}
+	}
+	public static void test(){
+		AnimalRoles a = new AnimalRoles();
+		a.original.hello();
+		assertEquals("Yap", "Rigid", a.out);
+		a.hello();
+		assertEquals("Yap", "Role Portuguese", a.out);
+		a.human = null;
+		a.hello();
+		assertEquals("Yap", "Role Bonobo", a.out);
+	}
+	
+}
